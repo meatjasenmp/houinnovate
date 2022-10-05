e24e5e0888333af598b8bccb84b0eae5ba4cf035
ntwrkjasen <jasen@thentwrk.com>
2022-10-05 06:07:51 -0500

    Updating ContentEditor.tsx to allow for link target.

--- components/ContentEditor.tsx
+++ components/ContentEditor.tsx
@@ -3,6 +3,12 @@ import styles from "../styles/components/ContentEditor.module.css";
 
 const ContentEditor = ({ content }: { content: any }) => {
   const clean = DOMPurify.sanitize(content);
+  DOMPurify.addHook("afterSanitizeAttributes", function (node) {
+    // set all elements owning target to target=_blank
+    if ("target" in node) {
+      node.setAttribute("target", "_blank");
+    }
+  });
   return (
     <div
       className={styles.content__editor}
