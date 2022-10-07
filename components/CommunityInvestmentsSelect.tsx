8a300f02c1bbe7afe359d1506f5861a1e0ac3faf
ntwrkjasen <jasen@thentwrk.com>
2022-10-06 19:49:11 -0500

    Basics of select implementation

--- components/CommunityInvestmentsSelect.tsx
+++ components/CommunityInvestmentsSelect.tsx
@@ -1,19 +1,35 @@
 import PopUpLink from "./PopUpLink";
+import Select from "react-select";
 import { useCommunityInvestmentsSelect } from "../pages/api/community_investments_select";
 
 import styles from "../styles/components/PopUpSelect.module.css";
+interface options {
+  value?: string | null | undefined;
+  label?: string | null | undefined;
+}
 
 const CommunityInvestmentsSelect = () => {
   const { data, loading, error } = useCommunityInvestmentsSelect();
   if (loading || error || !data) return <></>;
 
-  const { communityInvestments } = data;
+  const { communityInvestments, communityInvestmentTypes } = data;
+  const optionsArray: options[] = [];
+
+  communityInvestmentTypes?.edges?.map((investmentType) => {
+    const { node } = investmentType || {};
+    const { name } = node || {};
+    optionsArray.push({ value: name, label: name });
+  });
+
   return (
     <section>
-      {communityInvestments?.edges &&
-        communityInvestments.edges.map((link, index) => (
-          <PopUpLink link={link} key={index} />
-        ))}
+      <>
+        <Select options={optionsArray} />
+        {communityInvestments?.edges &&
+          communityInvestments.edges.map((link, index) => (
+            <PopUpLink link={link} key={index} />
+          ))}
+      </>
     </section>
   );
 };
