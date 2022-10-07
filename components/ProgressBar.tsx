b4504026fdf1852690ef762f5796893441760047
ntwrkjasen <jasen@thentwrk.com>
2022-10-07 15:38:28 -0500

    basic progressbar percentage

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -10,6 +10,14 @@ interface ProgressBarProps {
   accent: Colors;
 }
 
+const calculatePercentage = (
+  deployed: number | null | undefined,
+  committed: number | null | undefined
+) => {
+  if (!deployed || !committed) return 0;
+  return (deployed / committed) * 100;
+};
+
 const ProgressBar = ({
   deployed,
   committed,
@@ -20,11 +28,15 @@ const ProgressBar = ({
   const progressBarDeployedClassNames = [
     styles.progress_bar__deployed,
     backgroundColorMapping(accent),
+    `w-[${calculatePercentage(deployed, committed)}%]`,
   ].join(" ");
 
   return (
     <div className={styles.progress_bar}>
-      <div className={progressBarDeployedClassNames} />
+      <div
+        className={progressBarDeployedClassNames}
+        style={{ width: `${calculatePercentage(deployed, committed)}%` }}
+      />
     </div>
   );
 };
