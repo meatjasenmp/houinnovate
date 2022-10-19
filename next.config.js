0fb4930ccf445325df0bf31e96e31406673053fd
ntwrkjasen <jasen@thentwrk.com>
2022-10-19 06:54:25 -0500

    Adding token for GSAP

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    GSAP: "d82d5cbf-73e7-45eb-bba8-5b4c5b29bc66",
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
