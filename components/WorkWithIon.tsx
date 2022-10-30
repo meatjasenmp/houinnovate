8abf29945fcd2f7c3e9d15c4cca98ee9a0cc2869
ntwrkjasen <jasen@thentwrk.com>
2022-10-30 13:53:40 -0500

    Misc. style updates

--- components/WorkWithIon.tsx
+++ components/WorkWithIon.tsx
@@ -2,15 +2,41 @@ import ContentEditor from "./ContentEditor";
 import JobSelect from "./JobSelect";
 import { page_page_components_componentBlocks_Page_Components_ComponentBlocks_WorkWithIon } from "../pages/api/__generated__/page";
 import styles from "../styles/components/WorkWithIon.module.css";
+import { useEffect, useRef } from "react";
 
 interface WorkWithIonProps {
   blockContent: page_page_components_componentBlocks_Page_Components_ComponentBlocks_WorkWithIon;
 }
 const WorkWithIon = ({ blockContent }: WorkWithIonProps) => {
+  const contentWrapper = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (contentWrapper.current) {
+      const content = contentWrapper.current;
+      const headers = content.querySelectorAll("h1");
+
+      headers.forEach((item) => {
+        const headerItem = item as HTMLElement;
+        const words = headerItem.innerText.split(" ");
+
+        for (let i = 0; i < words.length; i++) {
+          const word = words[i];
+
+          if (i === 0) {
+            headerItem.innerHTML = `<span class="underline text-underline-red">${word}</span>`;
+          } else {
+            headerItem.innerHTML += ` <span class="underline text-underline-red">${word}</span>`;
+          }
+        }
+      });
+    }
+  }, []);
   return (
     <div className={styles.work_with_ion} id={String(blockContent.scrollId)}>
-      <div className={styles.work_with_ion_wrapper}>
-        <ContentEditor content={blockContent.workWithIonContent} />
+      <div className={styles.work_with_ion_wrapper} ref={contentWrapper}>
+        <div className={styles.work_with_ion__content}>
+          <ContentEditor content={blockContent.workWithIonContent} />
+        </div>
         <JobSelect />
       </div>
     </div>
