20da5154d2038c2a76aa31e78bd615381c626c38
ntwrkjasen <jasen@thentwrk.com>
2022-11-26 14:34:56 -0600

    Smooth scroll implementation.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from "next/app";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { HubspotProvider } from "next-hubspot";
 import Layout from "../components/Layout";
-import React from "react";
+import React, { useEffect } from "react";
 import { relayStylePagination } from "@apollo/client/utilities";
 import { Toaster } from "react-hot-toast";
 import ReactModal from "react-modal";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { ScrollSmoother } from "gsap/dist/ScrollSmoother";
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -30,14 +32,30 @@ const client = new ApolloClient({
 ReactModal.setAppElement("#__next");
 
 function MyApp({ Component, pageProps }: AppProps) {
-  gsap.registerPlugin(ScrollTrigger);
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      ScrollSmoother.create({
+        smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+        effects: true, // looks for data-speed and data-lag attributes on elements
+        smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+      });
+    });
+    return () => {
+      ctx.revert();
+    };
+  }, []);
+
   return (
     <ApolloProvider client={client}>
       <HubspotProvider>
-        <Layout>
-          <Toaster />
-          <Component {...pageProps} />
-        </Layout>
+        <div id="smooth-wrapper">
+          <div id="smooth-content">
+            <Layout>
+              <Toaster />
+              <Component {...pageProps} />
+            </Layout>
+          </div>
+        </div>
       </HubspotProvider>
     </ApolloProvider>
   );
